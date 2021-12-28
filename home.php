01103ef6ec8092342127443e4b6f578f6aac9ecf
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-12-28 22:48:27 +0100

    adding files

---
+++ home.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/svg/logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Tedbree - Find the best candidates for your organisation.</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="header__wrapper">
+                <div class="header__logo-box">
+                    <a href="" class="header__logo-link">
+                        <img src="assets/svg/lob.svg" alt="Find jobs - for employers" class="header__logo">
+                    </a>
+                </div>
+                <div class="header__profile">
+                    <div class="header__notification">
+                        <a href="" class="header__link">
+                            <svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.3821 23.1112C10.8196 23.1011 11.2394 22.9366 11.5672 22.6469C11.8951 22.3571 12.11 21.9607 12.1738 21.5278H8.521C8.5866 21.9725 8.8115 22.3782 9.15385 22.6694C9.49619 22.9607 9.93268 23.1177 10.3821 23.1112Z" fill="white"/>
+                                <path d="M20.4583 18.632C19.7887 18.035 19.2024 17.3506 18.7153 16.5972C18.1835 15.5573 17.8648 14.4217 17.7778 13.257V9.8264C17.775 9.4097 17.7378 8.99394 17.6667 8.58335C17.1201 8.47409 16.5948 8.2771 16.1111 8.00002C16.2955 8.59366 16.3891 9.21174 16.3889 9.83335V13.2639C16.474 14.6848 16.8649 16.0705 17.5347 17.3264C18.0142 18.0861 18.583 18.7856 19.2292 19.4097H1.44444C2.09056 18.7856 2.65944 18.0861 3.13889 17.3264C3.80876 16.0705 4.1996 14.6848 4.28472 13.2639V9.8264C4.28107 9.02271 4.43598 8.2262 4.74058 7.48246C5.04519 6.73872 5.49351 6.06237 6.05987 5.49214C6.62623 4.92191 7.29951 4.46899 8.04116 4.15933C8.7828 3.84966 9.57825 3.68933 10.3819 3.68751C11.5582 3.68846 12.708 4.0363 13.6875 4.68751C13.5795 4.29098 13.5189 3.88304 13.5069 3.47224V3.03474C12.7818 2.67804 12.0029 2.44341 11.2014 2.34029V1.46529C11.2014 1.21941 11.1037 0.983606 10.9299 0.809745C10.756 0.635883 10.5202 0.538208 10.2743 0.538208C10.0284 0.538208 9.79262 0.635883 9.61876 0.809745C9.4449 0.983606 9.34722 1.21941 9.34722 1.46529V2.37501C7.55258 2.62818 5.91012 3.52206 4.72307 4.89163C3.53602 6.26121 2.88453 8.014 2.88889 9.8264V13.257C2.80191 14.4217 2.48318 15.5573 1.95139 16.5972C1.47283 17.3489 0.895969 18.0332 0.236111 18.632C0.162036 18.697 0.102668 18.7771 0.0619565 18.8669C0.0212455 18.9567 0.000124858 19.0542 0 19.1528V20.0972C0 20.2814 0.0731644 20.4581 0.203398 20.5883C0.333632 20.7185 0.510266 20.7917 0.694445 20.7917H20C20.1842 20.7917 20.3608 20.7185 20.491 20.5883C20.6213 20.4581 20.6944 20.2814 20.6944 20.0972V19.1528C20.6943 19.0542 20.6732 18.9567 20.6325 18.8669C20.5918 18.7771 20.5324 18.697 20.4583 18.632Z" fill="white"/>
+                                <path d="M18.7154 6.94444C20.633 6.94444 22.1876 5.38988 22.1876 3.47222C22.1876 1.55457 20.633 0 18.7154 0C16.7977 0 15.2432 1.55457 15.2432 3.47222C15.2432 5.38988 16.7977 6.94444 18.7154 6.94444Z" fill="#EA5566"/>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="header__avatar-box">
+                        <img src="assets/images/avatar.png" alt="avatar" class="header__avatar">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="circle circle--1">
+            &nbsp;
+        </div>
+        <div class="circle circle--2">
+            &nbsp;
+        </div>
+        <div class="circle circle--3">
+            &nbsp;
+        </div>
+        <div class="container ">
+            <h1 class="heading heading--primary ">
+                Jobs
+            </h1>
+        </div>
+    </header>
+    <section class="section-jobs">
+        <div class="container">
+            <div class="jobs">
+                <div class="jobs__search">
+                    <form action="#" class="jobs__form">
+                        <div class="jobs__form-group">
+                            <span class="search-icon">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M25 23.6925L18.0167 16.7091C19.6948 14.6945 20.5316 12.1104 20.3531 9.49452C20.1745 6.8786 18.9943 4.43223 17.058 2.66432C15.1216 0.896406 12.5782 -0.0569296 9.95692 0.00263186C7.33559 0.0621934 4.83814 1.13007 2.9841 2.9841C1.13007 4.83814 0.0621934 7.33559 0.00263186 9.95692C-0.0569296 12.5782 0.896406 15.1216 2.66432 17.058C4.43223 18.9943 6.8786 20.1745 9.49452 20.3531C12.1104 20.5316 14.6945 19.6948 16.7091 18.0167L23.6925 25L25 23.6925ZM1.88247 10.2048C1.88247 8.55878 2.37056 6.94975 3.28503 5.58115C4.1995 4.21256 5.49927 3.14586 7.01997 2.51597C8.54067 1.88607 10.214 1.72126 11.8284 2.04238C13.4428 2.3635 14.9256 3.15612 16.0895 4.32002C17.2534 5.48391 18.0461 6.96681 18.3672 8.58118C18.6883 10.1955 18.5235 11.8689 17.8936 13.3896C17.2637 14.9103 16.197 16.2101 14.8284 17.1245C13.4598 18.039 11.8508 18.5271 10.2048 18.5271C7.99832 18.5246 5.88293 17.647 4.32272 16.0868C2.76252 14.5266 1.88492 12.4112 1.88247 10.2048Z" fill="#62BECB"/>
+                                </svg>
+                            </span>
+                            <input type="text" class="jobs__input" name="job-search" placeholder="Front end developer">
+                            <button type="submit" class="btn btn--search">Search</button>
+                        </div>
+                    </form>
+                    <div class="jobs__create">
+                        <button class="btn btn--add" type="submit">
+                            <span class="add-icon">
+                                <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12.5 12H4.5M12.5 20V12V20ZM12.5 12V4V12ZM12.5 12H20.5H12.5Z" stroke="white" stroke-width="2" stroke-linecap="round"/>
+                                </svg>
+                            </span>
+                            <span>
+                                New Job
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="jobs__board">
+                    <div class="jobs__header">
+                        <p class="jobs__paragraph">
+                            Job title
+                        </p>
+                        <p class="jobs__paragraph">
+                            Date modified
+                        </p>
+                        <p class="jobs__paragraph">
+                            Candidates
+                        </p>
+                        <button class="btn btn--filter">
+                            <span>
+                                Filter
+                            </span>
+                            <span class="filter-icon">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M22 3H2L10 12.46V19L14 21V12.46L22 3Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </span>
+                        </button>
+                    </div>
+                    <ul class="jobs__list">
+                        <li class="jobs__item">
+                            <span class="jobs__bullet">&nbsp;</span>
+                            <div class="jobs__details">
+                                <p class="jobs__text">
+                                    Customer sucess intern
+                                </p>
+                                <p class="jobs__text">
+                                    12/7/21
+                                </p>
+                                <p class="jobs__text">
+                                    50
+                                </p>
+                                <button class="btn btn--edit"> Edit </button>
+                                <button class="btn btn--delete"> Delete </button>
+                            </div>
+                        </li>
+                        <li class="jobs__item">
+                            <span class="jobs__bullet">&nbsp;</span>
+                            <div class="jobs__details">
+                                <p class="jobs__text">
+                                    Customer sucess intern
+                                </p>
+                                <p class="jobs__text">
+                                    12/7/21
+                                </p>
+                                <p class="jobs__text">
+                                    50
+                                </p>
+                                <button class="btn btn--edit"> Edit </button>
+                                <button class="btn btn--delete"> Delete </button>
+                            </div>
+                        </li>
+                        <li class="jobs__item">
+                            <span class="jobs__bullet">&nbsp;</span>
+                            <div class="jobs__details">
+                                <p class="jobs__text">
+                                    Customer sucess intern
+                                </p>
+                                <p class="jobs__text">
+                                    12/7/21
+                                </p>
+                                <p class="jobs__text">
+                                    50
+                                </p>
+                                <button class="btn btn--edit"> Edit </button>
+                                <button class="btn btn--delete"> Delete </button>
+                            </div>
+                        </li>
+                        <li class="jobs__item">
+                            <span class="jobs__bullet">&nbsp;</span>
+                            <div class="jobs__details">
+                                <p class="jobs__text">
+                                    Customer sucess intern
+                                </p>
+                                <p class="jobs__text">
+                                    12/7/21
+                                </p>
+                                <p class="jobs__text">
+                                    50
+                                </p>
+                                <button class="btn btn--edit"> Edit </button>
+                                <button class="btn btn--delete"> Delete </button>
+                            </div>
+                        </li>
+                         <li class="jobs__item">
+                            <span class="jobs__bullet">&nbsp;</span>
+                            <div class="jobs__details">
+                                <p class="jobs__text">
+                                    Customer sucess intern
+                                </p>
+                                <p class="jobs__text">
+                                    12/7/21
+                                </p>
+                                <p class="jobs__text">
+                                    50
+                                </p>
+                                <button class="btn btn--edit"> Edit </button>
+                                <button class="btn btn--delete"> Delete </button>
+                            </div>
+                        </li>
+                         <li class="jobs__item">
+                            <span class="jobs__bullet">&nbsp;</span>
+                            <div class="jobs__details">
+                                <p class="jobs__text">
+                                    Customer sucess intern
+                                </p>
+                                <p class="jobs__text">
+                                    12/7/21
+                                </p>
+                                <p class="jobs__text">
+                                    50
+                                </p>
+                                <button class="btn btn--edit"> Edit </button>
+                                <button class="btn btn--delete"> Delete </button>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="container">
+            &nbsp;
+        </div>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/svg/logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Tedbree - Find the best candidates for your organisation.</title>
+</head>
+<body>
+    <div class="login">
+        <div class="login__wrapper">
+            <div class="login__content">
+                <div class="login__overlay">
+                    <div class="login__logo-box">
+                        <a href="/" class="login__link">
+                            <img src="assets/svg/lob.svg" alt="Find jobs - for employers" class="login__logo">
+                        </a>
+                    </div>
+                    <h1 class="login__heading">
+                        <span>
+                            Find the best
+                        </span> 
+                        <span>
+                            candidates for your
+                        </span>
+                        <span>
+                            organisation.
+                        </span>
+                    </h1>
+                    <div class="login__hero">
+                        <img src="assets/images/login-hero.png" alt="login hero" class="login__image">
+                    </div>
+                </div>
+            </div>
+            <div class="login__form">
+                <div class="circle circle--1">
+                    &nbsp;
+                </div>
+                <div class="circle circle--2">
+                    &nbsp;
+                </div>
+                <div class="circle circle--3">
+                    &nbsp;
+                </div>
+                <form action="#" class="form">
+                    <h1 class="form__header">
+                        Login
+                    </h1>
+                    <div class="form__group">
+                        <label for="email" class="form__label">
+                            Email
+                        </label>
+                        <input type="email" class="form__input" id="email" name="email">
+                    </div>
+                    <div class="form__group">
+                        <label for="password" class="form__label">
+                            Password
+                        </label>
+                        <input type="password" class="form__input" id="password" name="password">
+                    </div>
+                    <div class="form__group">
+                        <button type="submit" class="btn btn--submit">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
